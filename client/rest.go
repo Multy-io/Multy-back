08ecb71886c88e67c729b9e704278be447f326bc
Eugene Boltenko <jekabolt@yahoo.com>
2018-01-26 18:37:57 +0300

    Test commit

--- client/rest.go
+++ client/rest.go
@@ -577,6 +577,7 @@ func reverse(ss []string) {
 	last := len(ss) - 1
 	for i := 0; i < len(ss)/2; i++ {
 		ss[i], ss[last-i] = ss[last-i], ss[i]
+		//
 	}
 }
 func resyncAddress(hash, RemoteAdd string, restClient *RestClient) {
