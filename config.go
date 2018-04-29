da657af7a772454c999a2b889849a71fcb18cd02
jekabolt <123gribochek123@gmail.com>
2018-04-30 01:16:56 +0300

    Docker fixes

--- config.go
+++ config.go
@@ -6,24 +6,24 @@ See LICENSE for details
 package multyback
 
 import (
-	"github.com/Appscrunch/Multy-back-exchange-service/core"
+	// "github.com/Appscrunch/Multy-back-exchange-service/core"
 	"github.com/Appscrunch/Multy-back/client"
 	"github.com/Appscrunch/Multy-back/store"
 )
 
 // Configuration is a struct with all service options
 type Configuration struct {
-	Name                   string
-	Database               store.Conf
-	SocketioAddr           string
-	RestAddress            string
-	Firebase               client.FirebaseConf
-	NSQAddress             string
-	BTCNodeAddress         string
-	DonationAddresses      []store.DonationInfo
-	MultyVerison           store.ServerConfig
-	ExchangerConfiguration core.ManagerConfiguration
-	ServicesInfo           []store.ServiceInfo
+	Name              string
+	Database          store.Conf
+	SocketioAddr      string
+	RestAddress       string
+	Firebase          client.FirebaseConf
+	NSQAddress        string
+	BTCNodeAddress    string
+	DonationAddresses []store.DonationInfo
+	MultyVerison      store.ServerConfig
+	// ExchangerConfiguration core.ManagerConfiguration
+	ServicesInfo []store.ServiceInfo
 
 	SupportedNodes []store.CoinType
 }
