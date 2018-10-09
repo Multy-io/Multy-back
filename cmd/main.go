aa86013952800304e5da83d4227dc1a18afac434
jekabolt <123gribochek123@gmail.com>
2018-10-09 16:48:33 +0300

    Minore imports fixes

--- cmd/main.go
+++ cmd/main.go
@@ -8,11 +8,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/Multy-io/Multy-BTC-node-service"
-	"github.com/Multy-io/Multy-back/store"
 	"github.com/KristinaEtc/config"
 	"github.com/KristinaEtc/slf"
 	_ "github.com/KristinaEtc/slflog"
+	"github.com/Multy-io/Multy-BTC-node-service"
+	"github.com/Multy-io/Multy-back/store"
 )
 
 var (
@@ -30,7 +30,6 @@ var globalOpt = node.Configuration{
 
 func main() {
 	config.ReadGlobalConfig(&globalOpt, "multy configuration")
-	log.Error("--------------------------------new multy back server session")
 	log.Infof("CONFIGURATION=%+v", globalOpt)
 	log.Infof("branch: %s", branch)
 	log.Infof("commit: %s", commit)
